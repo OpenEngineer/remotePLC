43682e642d10de2570aedebafa2f3d73e350f21b
christianschmitz <christian.schmitz@telenet.be>
2016-11-26 14:05:59 +0100

    cleaner exampleUdp in and output

--- blocks/ExampleUDP.go
+++ blocks/ExampleUDP.go
@@ -3,141 +3,134 @@ package blocks
 // code used by both ExampleUDPInput.go and ExampleUDPOutput.go
 
 import (
-  "../logger/"
-  "bytes"
-  "encoding/binary"
-  "fmt"
-  "log"
-  "net"
+	"../logger/"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"net"
 )
 
-// UDP protocol over port 1800 
-type ExampleUDPHeader1 struct {
-  Byte1 byte
-  Byte2 byte
-  Uint1 uint16
-}
+const (
+	EXAMPLEUDP_MAX_BYTES   int    = 1460 // the protocol must make sure that the EXAMPLEUDP_MAX_RECORDS can always fit in this
+	EXAMPLEUDP_MAX_RECORDS int    = 10
+	EXAMPLEUDP_DATA_SIZE   int    = 4
+	EXAMPLEUDP_PORT        string = "666"
+	EXAMPLEUDP_VERSION     byte   = 0x01
+	EXAMPLEUDP_READOP      byte   = 0x01
+	EXAMPLEUDP_WRITEOP     byte   = 0x02
+)
 
-type ExampleUDPHeader2 struct {
-  Uint1 uint16 // the number of records, TODO: rename everywhere to NRecords
-  Uint2 uint16 
+type ExampleUDPHeader struct {
+	Version    byte
+	OpId       byte // 0x01 for read, 0x02 for write
+	SeqNo      uint16
+	NumRecords uint16
+	ErrorCode  uint16 // 0 in case of no error, not used when sending a question
 }
 
 type ExampleUDPRecord struct {
-  Uint1 uint8 // example: counter
-  Uint2 uint16 // example: address on target machine
-  Uint3 uint8 // example: status byte
-  Data [4]byte // example: a float32
+	Id   uint16                     // example: id/address on target machine
+	Data [EXAMPLEUDP_DATA_SIZE]byte // example: a float32
 }
 
 type ExampleUDPPacket struct {
-  Header1 ExampleUDPHeader1
-  Header2 ExampleUDPHeader2
-  Records [10]ExampleUDPRecord // the max number of records
+	Header  ExampleUDPHeader
+	Records [EXAMPLEUDP_MAX_RECORDS]ExampleUDPRecord
+}
+
+func exampleUDPPacketSize(numRecords int) int {
+	numBytes := 8 + numRecords*(EXAMPLEUDP_DATA_SIZE+2)
+	return numBytes
 }
 
 func exampleUDPPacketToBytes(p ExampleUDPPacket) []byte {
-  b := new(bytes.Buffer)
-  err := binary.Write(b, binary.LittleEndian, p)
-  logger.WriteError("exampleUDPPacketToBytes()", err)
+	b := new(bytes.Buffer)
+	err := binary.Write(b, binary.LittleEndian, p)
+	logger.WriteError("exampleUDPPacketToBytes()", err)
 
-  // example: n records is stored somewhere in the second header
-  nRecords := int(p.Header2.Uint1)
-  nBytes := 4+4+nRecords*8
+	numRecords := int(p.Header.NumRecords)
+	numBytes := exampleUDPPacketSize(numRecords)
 
-  return b.Bytes()[0:nBytes]
+	return b.Bytes()[0:numBytes]
 }
 
 func exampleUDPBytesToPacket(b []byte) ExampleUDPPacket {
-  var p ExampleUDPPacket
-  buffer := bytes.NewBuffer(b)
-  binary.Read(buffer, binary.LittleEndian, &p)
+	var p ExampleUDPPacket
+	buffer := bytes.NewBuffer(b)
+	binary.Read(buffer, binary.LittleEndian, &p)
 
-  // TODO: handle errors here
-  return p
-} 
+	return p
+}
 
 var exampleUDPConnection net.Conn // shared by ExampleUDPInput and ExampleUDPOutput
 
 // a common port for sending and receiving
 func assertExampleUDPConnection(ipaddr string) {
-  port := "666"
+	if exampleUDPConnection == nil {
+		raddr, errRaddr := net.ResolveUDPAddr("udp", ipaddr+":"+EXAMPLEUDP_PORT)
+		logger.WriteError("assertExampleUDPConnection()", errRaddr)
 
-  if exampleUDPConnection == nil {
-    // resolve the addresses
-    raddr, errRaddr := net.ResolveUDPAddr("udp", ipaddr+":"+port)
-    if errRaddr != nil { 
-      log.Fatal(errRaddr) 
-    }
+		laddr, errLaddr := net.ResolveUDPAddr("udp", ":"+EXAMPLEUDP_PORT)
+		logger.WriteError("assertExampleUDPConnection()", errLaddr)
 
-    laddr, errLaddr := net.ResolveUDPAddr("udp", ":"+port)
-    if errLaddr != nil {
-      log.Fatal(errLaddr)
-    }
-
-    var errDial error
-    exampleUDPConnection, errDial = net.DialUDP("udp", laddr, raddr)
-    if errDial != nil {
-      log.Fatal(errDial)
-    }
-  }
+		var errDial error
+		exampleUDPConnection, errDial = net.DialUDP("udp", laddr, raddr)
+		logger.WriteError("assertExampleUDPConnection()", errDial)
+	}
 }
 
 // handle the carried 4 byte data
-var ExampleUDPBytesToFloat = make(map[string]func([4]byte)float64)
+var ExampleUDPBytesToFloat = make(map[string]func([EXAMPLEUDP_DATA_SIZE]byte) float64)
 
-var ExampleUDPFloatToBytes = make(map[string]func(float64)[4]byte)
+var ExampleUDPFloatToBytes = make(map[string]func(float64) [EXAMPLEUDP_DATA_SIZE]byte)
 
-func AddExampleUDPBytesToFloat(key string, fn func([4]byte)float64) bool {
-  ExampleUDPBytesToFloat[key] = fn
-  return true
+func AddExampleUDPBytesToFloat(key string, fn func([EXAMPLEUDP_DATA_SIZE]byte) float64) bool {
+	ExampleUDPBytesToFloat[key] = fn
+	return true
 }
 
-func AddExampleUDPFloatToBytes(key string, fn func(float64)[4]byte) bool {
-  ExampleUDPFloatToBytes[key] = fn
-  return true
+func AddExampleUDPFloatToBytes(key string, fn func(float64) [EXAMPLEUDP_DATA_SIZE]byte) bool {
+	ExampleUDPFloatToBytes[key] = fn
+	return true
 }
 
-func ExampleUDPType1BytesToFloat(b [4]byte) float64 {
-  // example: the last 2 byte are read in little endian fashion into an int16, the first two are ignored
-  // finally convert literally to float64
+// bytes 2 and 3 -> littleEndian uint16 -> float64
+func ExampleUDPType1BytesToFloat(b [EXAMPLEUDP_DATA_SIZE]byte) float64 {
+	buffer := bytes.NewBuffer(b[2:4])
+	var tmp int16
+	binary.Read(buffer, binary.LittleEndian, &tmp)
 
-  buffer := bytes.NewBuffer(b[2:])
-  var tmp int16
-  binary.Read(buffer, binary.LittleEndian, &tmp)
+	x := float64(tmp)
 
-  x := float64(tmp)
-
-  return x
+	return x
 }
 
 var ExampleUDPType1BytesToFloatOk = AddExampleUDPBytesToFloat("Type1", ExampleUDPType1BytesToFloat)
 
-// TODO: write inverse function for Type1: func(float64)[4]byte
+func ExampleUDPType2BytesToFloat(b [EXAMPLEUDP_DATA_SIZE]byte) float64 {
+	// Take first two bytes instead of 2 and 3
+	buffer := bytes.NewBuffer(b[0:2])
+	var tmp uint16
+	binary.Read(buffer, binary.LittleEndian, &tmp)
 
-func ExampleUDPType2BytesToFloat(b [4]byte) float64 {
-  // Take first two bytes instead of last two bytes
-  buffer := bytes.NewBuffer(b[0:2])
-  var tmp uint16
-  binary.Read(buffer, binary.LittleEndian, &tmp)
-
-  x := float64(tmp)
-  return x
+	x := float64(tmp)
+	return x
 }
 
 var ExampleUDPType2BytesToFloatOk = AddExampleUDPBytesToFloat("Type2", ExampleUDPType2BytesToFloat)
 
-func ExampleUDPType2FloatToBytes(x float64) (b [4]byte) {
-  // convert to uint16
-  tmp := uint16(x)
-  buffer := new(bytes.Buffer)
-  err := binary.Write(buffer, binary.LittleEndian, tmp)
-  logger.WriteError("ExampleUDPType2FloatToBytes()", err)
+func ExampleUDPType2FloatToBytes(x float64) (b [EXAMPLEUDP_DATA_SIZE]byte) {
+	// convert to uint16
+	tmp := uint16(x)
+	buffer := new(bytes.Buffer)
+	err := binary.Write(buffer, binary.LittleEndian, tmp)
+	logger.WriteError("ExampleUDPType2FloatToBytes()", err)
 
-  b[0] = buffer.Bytes()[0]
-  b[1] = buffer.Bytes()[1]
-  fmt.Println(b, x)
-  return
+	b[0] = buffer.Bytes()[0]
+	b[1] = buffer.Bytes()[1]
+	fmt.Println(b, x)
+	return
 }
 
 var ExampleUDPType2FloatToBytesOk = AddExampleUDPFloatToBytes("Type2", ExampleUDPType2FloatToBytes)
--- blocks/ExampleUDPOutput.go
+++ blocks/ExampleUDPOutput.go
@@ -1,106 +1,109 @@
 package blocks
 
 import (
-  "../logger/"
-  "errors"
-  "fmt"
-  "net"
-  "log"
-  "strconv"
-  "strings"
+	"../logger/"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"strconv"
+	"strings"
 )
 
 // see ExampleUDP.go for protocol details
 
 type ExampleUDPOutput struct {
-  OutputBlockData
-  nRecords int // fixed, the nRecords value in question is varied
-  question ExampleUDPPacket // converted to []byte in Update()
-
-  converter func(float64)[4]byte
-
-  conn net.Conn
+	OutputBlockData
+	numRecords int              // fixed here, because the numRecords value in question is varied
+	question   ExampleUDPPacket // filled and converted to []byte in Update()
+	dataConv   func(float64) [EXAMPLEUDP_DATA_SIZE]byte
+	conn       net.Conn
 }
 
 func (b *ExampleUDPOutput) Update() {
-  // If the b.in contains less elements than nRecords, then nRecords is set to len(b.in)
-  nRecords := b.nRecords
-  if len(b.in) < b.nRecords {
-    nRecords = len(b.in)
-  }
+	// If the b.in contains less elements than numRecords, then numRecords is set to len(b.in)
+	numRecords := b.numRecords
+	if len(b.in) < b.numRecords {
+		numRecords = len(b.in)
+	}
 
-  // Now set the nSdos in question
-  b.question.Header1.Uint1 = uint16(nRecords)
+	// Now set the nSdos in question
+	b.question.Header.NumRecords = uint16(numRecords)
 
-  // loop the input and store the records
-  for i, v := range b.in {
-    b.question.Records[i].DwData = b.converter(v)
-  }
+	// loop the input and store the records
+	for i, v := range b.in {
+		b.question.Records[i].Data = b.dataConv(v)
+	}
 
-  questionBytes := exampleUDPPacketToBytes(b.question)
+	questionBytes := exampleUDPPacketToBytes(b.question)
 
-  _, errWrite := b.conn.Write(msg)
-  logger.WriteError("ExampleUDPOutput.Update()", errWrite)
+	_, errWrite := b.conn.Write(msg)
+	logger.WriteError("ExampleUDPOutput.Update()", errWrite)
 
-  answerBytes := make([]byte, 1460)
-  _, err := b.conn.Read(answerBytes)
-  logger.WriteError("ExampleUDPOutput.Update()", err)
+	answerBytes := make([]byte, 1460)
+	_, err := b.conn.Read(answerBytes)
+	logger.WriteError("ExampleUDPOutput.Update()", err)
 
-  // now parse the return message
-  answer := exampleUDPBytesToPacket(answerBytes)
-  // TODO: check the status of the answer
+	// now parse the return message
+	answer := exampleUDPBytesToPacket(answerBytes) // TODO: create the error herein
+	if answer.Header.ErrorCode != 0 {
+		logger.WriteError("ExampleUDPOutput.Update()",
+			errors.New("protocol error "+strconv.Itoa(answer.Header.ErrorCode)))
+	}
 
-  b.out = b.in[0:nRecords]
-  return
+	b.out = b.in[0:numRecords]
+	return
 }
 
 func ExampleUDPOutputConstructor(words []string) Block {
-  ipaddr := words[0]
-  converterType := words[1]
-  recordWords := words[2:]
+	ipaddr := words[0]
+	dataConvType := words[1]
+	recordIds := words[2:]
 
-  question := ExampleUDPPacket{
-    Header1: ExampleUDPHeader1{
-      Byte2: 0xbb,
-    },
-    Header2: ExampleUDPHeader2{
-      Uint2: 10,
-    },
-  }
+	question := ExampleUDPPacket{
+		Header: ExampleUDPHeader{
+			Version: EXAMPLEUDP_VERSION,
+			OpId:    EXAMPLEUDP_WRITEOP,
+			SeqNo:   uint16(1),
+		},
+	}
 
-  // store all the records
-  nRecords := len(recordWords)
-  if nRecords == 0 {
-    log.Fatal("must specify at least one record")
-  } else if nRecords > 10 {
-    log.Fatal("too many records")
-  }
+	// store all the records
+	numRecords := len(recordIds)
+	if numRecords == 0 {
+		logger.WriteError("ExampleUDPOutputConstructor()",
+			errors.New("must specify at least one record"))
+	} else if numRecords > EXAMPLEUDP_MAX_RECORDS {
+		logger.WriteError("ExampleUDPOutputConstructor()",
+			errors.New("too many records"))
+	}
 
-  question.Header2.Uint1 = uint16(nRecords)
-  for i, w := range recordWords {
-    id, errId := strconv.ParseUint(w, 0, 16)
-    logger.WriteError("ExampleUDPOutputConstructor()", errId)
+	question.Header.NumRecords = uint16(numRecords)
+	for i, idStr := range recordIds {
+		id, errId := strconv.ParseUint(idStr, 0, 16)
+		logger.WriteError("ExampleUDPOutputConstructor()", errId)
 
-    question.Records[i] = ExampleUDPRecord{
-      Uint2: uint16(id),
-    }
-  }
+		question.Records[i] = ExampleUDPRecord{
+			Id: uint16(id),
+		}
+	}
 
-  assertExampleUDPConnection(ipaddr)
+	assertExampleUDPConnection(ipaddr)
 
-  converter, ok := ExampleUDPFloatToBytes[converterType]
-  if !ok {
-    logger.WriteError("ExampleUDPOutputConstructor()", errors.New("couldnt find "+converterType)) // TODO: also do this for the Input constructor
-  }
+	dataConv, ok := ExampleUDPFloatToBytes[dataConvType]
+	if !ok {
+		logger.WriteError("ExampleUDPOutputConstructor()",
+			errors.New("couldnt find "+dataConvType)) // TODO: also do this for the Input constructor
+	}
 
-  b := &ExampleUDPOutput{
-    question: question,
-    conn: exampleUDPConnection, 
-    converter: converter,
-    nRecords: nRecords,
-  }
+	b := &ExampleUDPOutput{
+		question:   question,
+		dataConv:   dataConv,
+		numRecords: numRecords,
+		conn:       exampleUDPConnection,
+	}
 
-  return b
+	return b
 }
 
 var ExampleUDPOutputConstructorOk = AddConstructor("ExampleUDPOutput", ExampleUDPOutputConstructor)
