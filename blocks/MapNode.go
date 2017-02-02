81279f913eb6f2eb1a4424b2724a04869c748349
Christian Schmitz <christian.schmitz@telenet.be>
2017-02-02 22:36:23 +0100

    multiD interpolation support, exact or undefined mode, inputs no longer need to be sorted

--- blocks/MapNode.go
+++ blocks/MapNode.go
@@ -3,12 +3,10 @@ package blocks
 import (
 	"../logger/"
 	"../parser/"
-	"bufio"
 	"errors"
-	"log"
+	"math"
 	"os"
 	"strconv"
-	"strings"
 	"time"
 )
 
@@ -17,97 +15,95 @@ import (
 
 type MapNode struct {
 	BlockData
+
+	fname   string
 	file    *os.File // usefull for seeking
 	modTime time.Time
-	xInterp []float64
-	yInterp [][]float64
+
+	numInput  int
+	numOutput int
+	xInterp   [][]float64
+	yInterp   [][]float64
 
 	// options
-	cycle    bool
-	discrete bool // do not interpolate (nearest behaviour)
-}
-
-// TODO: log error messages
-func checkMonotonicity_(xInterp []float64) bool {
-	isMonotone := true
-
-	for i, x := range xInterp[1:] {
-		if x < xInterp[i] {
-			isMonotone = false
-		}
-	}
-
-	return isMonotone
-}
-
-func (b *MapNode) getFname() string {
-	info, _ := b.file.Stat()
-	fname := info.Name()
-	return fname
+	mode string // defaults to "interpolate", other options are "nearest" and "exact"
 }
 
 // Example of runtime file modification
 func (b *MapNode) readFile() error {
-	if b.file == nil {
-		return errors.New("error: MapNode nil")
-	}
-	b.file.Seek(0, 0) // also seeks to 0 when piping stdin
+	tokens := parser.TokenizeFile(b.fname)
 
-	scanner := bufio.NewScanner(b.file) // use default Split: ScanLines
+	// temporary storage to check for validity
+	xInterpTmp := [][]float64{}
+	yInterpTmp := [][]float64{}
 
-	logger.WriteEvent("reading MapNode file ", b.getFname())
+	isOk := true
+	for _, l := range tokens {
+		// row data
+		xInterpRow := []float64{}
+		yInterpRow := []float64{}
+		if len(l) == b.numInput+b.numOutput {
+			for i := 0; i < b.numInput; i++ {
+				xInterp, err := strconv.ParseFloat(l[i], 64)
+				if err != nil {
+					isOk = false
+					break
+				}
+				xInterpRow = append(xInterpRow, xInterp)
+			}
 
-	xInterpNew := []float64{}
-	yInterpNew := [][]float64{}
+			if !isOk {
+				break
+			}
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		words := strings.Fields(line)
+			for i := b.numInput; i < b.numOutput+b.numInput; i++ {
+				yInterp, err := strconv.ParseFloat(l[i], 64)
+				if err != nil {
+					isOk = false
+					break
+				}
+				yInterpRow = append(yInterpRow, yInterp)
+			}
 
-		// parse the time
-		xInterp, _ := strconv.ParseFloat(words[0], 64)
-		xInterpNew = append(xInterpNew, xInterp)
+			if !isOk {
+				break
+			}
 
-		// parse the values
-		yInterp := []float64{}
-		for _, word := range words[1:] {
-			y, _ := strconv.ParseFloat(word, 64)
-			yInterp = append(yInterp, y)
+		} else {
+			isOk = false
+			break
 		}
-		yInterpNew = append(yInterpNew, yInterp)
+
+		xInterpTmp = append(xInterpTmp, xInterpRow)
+		yInterpTmp = append(yInterpTmp, yInterpRow)
 	}
 
-	// change the block only if the values are monotone
-	if len(xInterpNew) == 0 {
-		return errors.New("file NOK")
-	} else if checkMonotonicity_(xInterpNew) {
-		b.xInterp = xInterpNew
-		b.yInterp = yInterpNew
+	if !isOk {
+		return errors.New("warnng: invalid MapNode, ignoring")
+	} else {
+		b.xInterp = xInterpTmp
+		b.yInterp = yInterpTmp
+
 		info, _ := b.file.Stat()
 		b.modTime = info.ModTime()
-	} else {
-		return errors.New("warning: invalid MapNode, ignoring")
+		return nil
 	}
-
-	return nil
 }
 
 // it is assumed that the parsing the file takes some time
 // so we check if it has had an update, this requires "reopening" the file though
 func (b *MapNode) refreshFile() {
-	fname := b.getFname()
-
 	// reread the time file (i.e. update Stat)
 	var openErr error
-	b.file, openErr = reopenFile_(fname, b.file)
+	b.file, openErr = reopenFile_(b.fname, b.file)
 	if openErr != nil {
-		logger.WriteEvent("warning ignoring timefile ", fname, ", ", openErr)
+		logger.WriteEvent("warning ignoring timefile ", b.fname, ", ", openErr)
 		return
 	}
 
 	info, statErr := b.file.Stat()
 	if statErr != nil {
-		logger.WriteEvent("warning ignoring timefile ", fname, ", ", statErr)
+		logger.WriteEvent("warning ignoring timefile ", b.fname, ", ", statErr)
 		return
 	}
 
@@ -119,85 +115,174 @@ func (b *MapNode) refreshFile() {
 	}
 }
 
-func (b *MapNode) findInterpSlice(x float64) (int, int, float64) {
-	// find the lower index
-	if b.cycle {
-		xPeriod := b.xInterp[len(b.xInterp)-1] - b.xInterp[0]
-		if x > xPeriod {
-			x = x - xPeriod*float64(int((x-b.xInterp[0])/xPeriod))
-		} else if x < b.xInterp[0] {
-			x = x + xPeriod*float64(int((x-b.xInterp[0])/xPeriod)+1)
+// linear interpolation between the two closest bounding numbers (or clipped if no upper or lower bounds found)
+func (b *MapNode) Interp1D(x float64) []float64 {
+	// find the numbers the are closest and smaller, and closer and bigger
+	iLower := -1
+	dMinLower := 1e300
+
+	iUpper := -1
+	dMinUpper := 1e300
+
+	for i, v := range b.xInterp {
+		dLower := x - v[0]
+		if dLower >= 0 && dLower < dMinLower {
+			dMinLower = dLower
+			iLower = i
 		}
 
-		if x > xPeriod { // TODO: check and remove this
-			log.Fatal("bad calc in findInterpSlice()")
+		dUpper := v[0] - x
+		if dUpper > 0 && dUpper < dMinUpper {
+			dMinUpper = dUpper
+			iUpper = i
 		}
 	}
 
-	var iLower int
-	var iUpper int
-	if x < b.xInterp[0] {
-		iLower = 0
-		iUpper = 0
+	yInterp := make([]float64, b.numOutput)
+
+	if iLower == -1 && iUpper == -1 {
+		for i, _ := range yInterp {
+			yInterp[i] = UNDEFINED
+		}
+	} else if iLower > -1 && iUpper == -1 {
+		yInterp = b.yInterp[iLower]
+	} else if iUpper > -1 && iLower == -1 {
+		yInterp = b.yInterp[iUpper]
 	} else {
-		for iLower = 0; iLower < len(b.xInterp)-1; iLower++ {
-			if b.xInterp[iLower] <= x && x < b.xInterp[iLower+1] {
+		// interpolation fraction
+		f := dMinLower / (dMinUpper + dMinLower)
+		for i, _ := range yInterp {
+			yInterp[i] = (1.0-f)*b.yInterp[iLower][i] + f*b.yInterp[iUpper][i]
+		}
+	}
+
+	return yInterp
+}
+
+func (b *MapNode) NearestND(x []float64) []float64 {
+	iNearest := -1
+	dMin := 1e300 // euclidian distance
+
+	// check which vector is closest
+	for i, v := range b.xInterp {
+		// loop the vector components
+		d := 0.0
+		for j, vv := range v {
+			d = d + (vv-x[j])*(vv-x[j])
+		}
+
+		if d < dMin {
+			dMin = d
+			iNearest = i
+		}
+	}
+
+	yInterp := make([]float64, b.numOutput)
+	if iNearest == -1 {
+		for i, _ := range yInterp {
+			yInterp[i] = UNDEFINED
+		}
+	} else {
+		yInterp = b.yInterp[iNearest]
+	}
+	return yInterp
+}
+
+// inverse distance weighting
+func (b *MapNode) InterpND(x []float64) []float64 {
+
+	// calculate the weights
+	w := make([]float64, len(b.xInterp))
+	wSum := 0.0
+	isExact := false
+	iExact := -1
+	for i, v := range b.xInterp {
+		d := 0.0
+		for j, vv := range v {
+			d = d + (vv-x[j])*(vv-x[j])
+		}
+
+		if d == 0.0 {
+			isExact = true
+			iExact = i
+			break
+		} else {
+			w[i] = math.Sqrt(1.0 / d)
+			wSum = wSum + w[i]
+		}
+	}
+
+	// calculate the interpolated vector
+	yInterp := make([]float64, b.numOutput)
+	if isExact {
+		yInterp = b.yInterp[iExact]
+	} else {
+		for i, v := range b.yInterp {
+			for j, vv := range v {
+				yInterp[j] = yInterp[j] + vv*w[i]/wSum
+			}
+		}
+	}
+
+	return yInterp
+}
+
+func (b *MapNode) Exact(x []float64) []float64 {
+	isExact := false
+	iExact := -1
+
+	for i, v := range b.xInterp {
+		isExactLocal := true
+		for j, vv := range v {
+			if vv != x[j] {
+				isExactLocal = false
 				break
 			}
 		}
 
-		// upper index
-		iUpper = iLower + 1
-		if iLower == len(b.xInterp)-1 {
-			iUpper = iLower
+		if isExactLocal {
+			isExact = true
+			iExact = i
+			break
 		}
 	}
 
-	// interpolation factor
-	dx := b.xInterp[iUpper] - b.xInterp[iLower]
-	var alpha float64
-	if dx != 0.0 {
-		alpha = (x - b.xInterp[iLower]) / dx
+	yInterp := make([]float64, b.numOutput)
+	if isExact {
+		yInterp = b.yInterp[iExact]
 	} else {
-		alpha = 1.0
+		for i, _ := range yInterp {
+			yInterp[i] = UNDEFINED
+		}
 	}
-
-	return iLower, iUpper, alpha
+	return yInterp
 }
 
 func (b *MapNode) Put(x []float64) {
-	if len(x) == 0 {
+	if len(x) != b.numInput {
 		return // don't do anything
 	}
 	b.refreshFile()
 
-	// find the time index
-	iLower, iUpper, alpha := b.findInterpSlice(x[0])
-	y0 := b.yInterp[iLower]
-	y1 := b.yInterp[iUpper]
-
+	// do different things depending on the settings
 	var yInterp []float64
 
-	// interpolate
-	if !b.discrete {
-		if len(y0) < len(y1) {
-			y0 = append(y0, make([]float64, len(y1)-len(y0))...)
-		} else if len(y1) < len(y0) {
-			y1 = append(y1, make([]float64, len(y0)-len(y1))...)
+	switch mode := b.mode; mode {
+	case "exact":
+		yInterp = b.Exact(x) // Undefined if not exact
+	case "nearest":
+		yInterp = b.NearestND(x)
+	case "interpolate":
+		if b.numInput == 1 {
+			yInterp = b.Interp1D(x[0])
+		} else {
+			yInterp = b.InterpND(x)
 		}
-
-		yInterp = make([]float64, len(y0))
-
-		for i := 0; i < len(yInterp); i++ { // in place modification
-			yInterp[i] = (1.0-alpha)*y0[i] + alpha*y1[i]
-		}
-	} else if alpha < 0.5 {
-		yInterp = y0
-	} else {
-		yInterp = y1
+	default:
+		logger.WriteError("MapNode.Put()", errors.New("MapNode mode not recognized"))
 	}
 
-	b.in = yInterp
+	b.in = x
 	b.out = yInterp
 }
 
@@ -219,23 +304,23 @@ func reopenFile_(fname string, file *os.File) (*os.File, error) {
 }
 
 func MapNodeConstructor(name string, words []string) Block {
-	var file *os.File
-	cycle := false
-	discrete := false
+	var fname string
+	mode := "interpolate" // or possible values are "nearest" and "exact"
 
-	positional := parser.PositionalArgs(&file)
-	optional := parser.OptionalArgs("Cycle", &cycle, "Discrete", &discrete)
+	positional := parser.PositionalArgs(&fname)
+	optional := parser.OptionalArgs("Mode", &mode)
 
 	parser.ParseArgs(words, positional, optional)
 
-	if cycle {
-		logger.WriteEvent("cycling MapNode")
-	}
+	var file *os.File
+	var err error
+	file, err = reopenFile_(fname, file)
+	logger.WriteError("MapNodeConstructor()", err)
 
-	b := &MapNode{file: file, cycle: cycle, discrete: discrete}
+	b := &MapNode{fname: fname, file: file, mode: mode}
 
 	readErr := b.readFile()
-	logger.WriteError("error in MapNodeConstructor: "+b.getFname()+" not in valid format", readErr)
+	logger.WriteError("error in MapNodeConstructor: "+b.fname+" not in valid format", readErr)
 
 	logger.WriteEvent("constructed MapNode " + name)
 	return b
